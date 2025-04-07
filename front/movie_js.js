7c46fb261e60d46be0b77f65bb47e05a7217d0be
sooho <soho5445@g.shingu.ac.kr>
2025-04-07 15:48:52 +0900

    4-7일 전부 반영

--- front/movie_js.js
+++ front/movie_js.js
@@ -43,7 +43,52 @@ let MovieObject = {
             // 실패
             console.log(error)
         })
-    }
+    },
+
+    getgenres: function() {
+        $.ajax({
+            // 실행 코드
+            type: "GET",
+            url: "http://localhost:8000/genresbest/" + document.getElementById("sgenre").value,
+        }).done(function(response) {
+            // 성공
+            movielist = response.result
+
+            // topdiv = document.getElementById("div")
+            // topdiv.style = "column-count:5"
+            // document.body.appendChild(topdiv)
+
+            topdiv = document.getElementById("genrediv")
+            topdiv.innerHTML = "";
+
+            movielist.forEach(movie => {
+
+                cmovie = document.createElement("div")
+                cmovie.className = "card"
+
+                mimg = document.createElement("img")
+                mimg.className = "card-img-top"
+                mimg.src = movie.poster_path
+                mimg.onclick = function() {
+                    // location.href 현재창에서 열기
+                    // window.open 새창으로 열기
+                    window.open(movie.url)
+                }
+
+                mimg.onmouseover = function(){
+                    mimg.style.cursor = "pointer"
+                }
+
+                cmovie.appendChild(mimg)
+                topdiv.appendChild(cmovie)
+            })
+        }).fail(function(error) {
+            // 실패
+            console.log(error)
+        })
+    },
+
+
 }
 
 MovieObject.init();
