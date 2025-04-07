834335942a82b5efccc2834c3b7a0d973ed31bd6
sooho <soho5445@g.shingu.ac.kr>
2025-04-07 14:25:35 +0900

    all 영화 추천 완성

---
+++ front/movie_js.js
@@ -0,0 +1,48 @@
+let MovieObject = {
+    init: function() {
+
+    },
+
+    getall: function() {
+        $.ajax({
+            // 실행 코드
+            type: "GET",
+            url: "http://localhost:8000/all",
+        }).done(function(response) {
+            // 성공
+            movielist = response.result
+
+            topdiv = document.getElementById("div")
+            topdiv.style = "column-count:5"
+            document.body.appendChild(topdiv)
+
+            movielist.forEach(movie => {
+
+                cmovie = document.createElement("div")
+                cmovie.className = "card"
+
+                mimg = document.createElement("img")
+                mimg.className = "card-img-top"
+                mimg.src = movie.poster_path
+                mimg.onclick = function() {
+                    // location.href 현재창에서 열기
+                    // window.open 새창으로 열기
+                    window.open(movie.url)
+                }
+
+                mimg.onmouseover = function(){
+                    mimg.style.cursor = "pointer"
+                }
+
+                cmovie.appendChild(mimg)
+                topdiv.appendChild(cmovie)
+            })
+        }).fail(function(error) {
+            // 실패
+            console.log(error)
+        })
+    }
+}
+
+MovieObject.init();
+MovieObject.getall();
